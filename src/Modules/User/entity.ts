3226efc0b68bb18bbf90d211a3810af647cf9fa1
Cristiano <neivacristiano@yahoo.com.br>
2022-01-30 18:35:51 -0300

    feat: add user basis entity

---
+++ src/Modules/User/entity.ts
@@ -0,0 +1,37 @@
+import { PrimaryGeneratedColumn, Column, BeforeUpdate, BeforeInsert } from 'typeorm';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ nullable: false })
+    name: string;
+
+    @Column({ unique: true, nullable: false })
+    email: string;
+
+    @Column({ nullable: false })
+    password: string;
+
+    public async compareHash(hash: string | Buffer): Promise<boolean> {
+        return bcrypt.compare(hash, this.password);
+    }
+
+    public generateToken(): string {
+        return jwt.sign({ id: this.id }, 'secret', {
+            expiresIn: 86400,
+        });
+    }
+
+    private async hashPassword(): Promise<void> {
+        if (this.password) this.password = await bcrypt.hash(this.password, 8);
+    }
+
+    @BeforeUpdate()
+    @BeforeInsert()
+    private async triggerBeforeSave(): Promise<void> {
+        await this.hashPassword();
+    }
+}
